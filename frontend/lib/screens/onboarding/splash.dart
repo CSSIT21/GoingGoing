d6cd1001439ed9287428bb39ca96ea0ae6c83d5f
muumel <mel.austapinya@gmail.com>
2022-06-14 18:26:38 +0700

    create splash screen and textfield
    textfield are label text formfield and password formfield

--- frontend/lib/screens/onboarding/splash.dart
+++ frontend/lib/screens/onboarding/splash.dart
@@ -1,11 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'dart:async' as async;
 
-class Splashscreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../config/themes/app_colors.dart';
+
+class Splashscreen extends StatefulWidget {
   const Splashscreen({Key? key}) : super(key: key);
 
   @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  _SplashscreenState() {
+    async.Timer(const Duration(milliseconds: 3000), () async {
+      final prefs = await SharedPreferences.getInstance();
+      String? user = prefs.getString('user');
+      if (user == null) {
+        Navigator.pushReplacementNamed(context, '/login');
+      } else {
+        Navigator.pushReplacementNamed(context, '/home');
+      }
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: AppColors.primaryColor,
+        body: Center(
+            child: Image.asset(
+          'assets/images/logo.png',
+          height: 100,
+          width: 100,
+        )));
   }
 }
