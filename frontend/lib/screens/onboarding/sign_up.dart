881560adf57ef3bddd911d11e5bfa993a4bfc82d
muumel <79404446+muumel@users.noreply.github.com>
2022-06-15 16:18:06 +0700

    Frontend login-and-signup mel (#3)
    * create login page
    
    create all element in login page and edit textfield
    
    * create login and sign up page
    
    create login, sig up page, and other textfield such as, datepicker field and dropdown field

--- frontend/lib/screens/onboarding/sign_up.dart
+++ frontend/lib/screens/onboarding/sign_up.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:going_going_frontend/widgets/common/date_picker_field.dart';
+import 'package:going_going_frontend/widgets/common/dropdown_field.dart';
+import '../../config/routes/routes.dart';
+import '../../config/themes/app_colors.dart';
+import '../../config/themes/app_text_theme.dart';
+import '../../widgets/common/label_textfield.dart';
+import '../../widgets/common/password_field.dart';
+import '../../widgets/login/login_title.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -10,6 +19,89 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 128, left: 32, bottom: 18),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[LoginTitle(titleText: 'Sign Up')],
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 1,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 32),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const LabelTextField(
+                            hintText: 'Enter your phone number',
+                            labelText: 'Phone Number'),
+                        const PasswordField(),
+                        const LabelTextField(
+                            hintText: 'Enter your firstname',
+                            labelText: 'Firstname'),
+                        const LabelTextField(
+                            hintText: 'Enter your lastname',
+                            labelText: 'Lastname'),
+                        const DatePickerField(labelText:'Date'),
+                        const DropdownField(hintText: 'Select your gender', labelText: 'Gender'),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          child: RichText(
+                            text: TextSpan(
+                                style: AppTextTheme.textTheme.subtitle1,
+                                children:  <TextSpan>[
+                                  const TextSpan(
+                                      text: "Already have an account? ",
+                                      style: TextStyle(color: Colors.grey)),
+                                  TextSpan(
+                                    text: "Log In",
+                                    style: const TextStyle(
+                                      color: AppColors.secondaryColor,
+                                    ),
+                                    recognizer: TapGestureRecognizer()
+                                        ..onTap = () {
+                                          Navigator.popAndPushNamed(
+                                              context, Routes.login);
+                                        })
+                                ]),
+                          ),
+                        ),
+                        
+                        //Add Button
+
+                      ],
+                    ),
+                  ),
+                ),
+              ]),
+        ),
+      ),
+    );
   }
 }
---
+++ frontend/lib/widgets/common/dropdown_field.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import '../../config/themes/app_colors.dart';
+
+class DropdownField extends StatefulWidget {
+  final String labelText;
+  final String hintText;
+
+  const DropdownField({required this.hintText, required this.labelText});
+
+  @override
+  _DropdownFieldState createState() => _DropdownFieldState();
+}
+
+class _DropdownFieldState extends State<DropdownField> {
+  List<String> gender = ['Select', 'Male', 'Female'];
+  String? selectedItem = 'Select';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 32, right: 32, top: 18, bottom: 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 8),
+            child: Text(
+              widget.labelText,
+              style: const TextStyle(color: Colors.grey),
+            ),
+          ),
+          SizedBox(
+            //width: 480,
+            height: 48,
+            child: DropdownButtonFormField<String>(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please select a transaction type';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide:
+                        const BorderSide(color: AppColors.primaryColor)),
+              ),
+              isDense: true,
+              value: selectedItem,
+              hint: Text(widget.hintText),
+              isExpanded: true,
+              items: gender.map((value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedItem = value;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
