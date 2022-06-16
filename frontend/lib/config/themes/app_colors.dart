dcba0113668eaa00642edaa067068b43d3247980
Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
2022-06-16 19:18:38 +0700

    config: app colors and text theme

--- frontend/lib/config/themes/app_colors.dart
+++ frontend/lib/config/themes/app_colors.dart
@@ -6,5 +6,7 @@ class AppColors {
   static const Color primaryColor = Color(0xFFFFD600);
   static const Color secondaryColor = Color(0xFFFF8900);
   static const Color black = Color(0xFF393939);
+  static const Color blackGrey = Color(0xFF898686);
   static const Color grey = Color(0xFFF1F2F6);
+  static const Color white = Color(0xFFFFFFFF);
 }
