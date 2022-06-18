3426ace70231163205df4c00d5017a2ed58373f3
Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
2022-06-19 00:01:35 +0700

    update: profile_section widget

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 // packages
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:going_going_frontend/screens/general/profile.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
@@ -51,7 +52,7 @@ class MyApp extends StatelessWidget {
         textTheme: AppTextTheme.textTheme,
       ),
       routes: Routes.routes,
-      home: const HomeScreen(),
+      home: const ProfileScreen(),
     );
   }
 }
