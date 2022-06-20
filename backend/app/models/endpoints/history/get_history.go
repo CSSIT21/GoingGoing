b0b834d05325b795f293d4a887e3b12218e3951e
Patiphon Klangpraphan <69670820+cympati@users.noreply.github.com>
2022-06-20 22:32:05 +0700

    update: backend by cream (#22)
    * update: schedule model
    
    * update: history endpoint
    
    Co-authored-by: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>

--- backend/app/models/endpoints/history/get_history.go
+++ backend/app/models/endpoints/history/get_history.go
@@ -1,5 +1,30 @@
 package history
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"going-going-backend/app/models/common"
+	"going-going-backend/platform/database"
+	"going-going-backend/platform/migrations"
+	"time"
+)
 
-func GetHandler(c *fiber.Ctx) error { return nil }
+func GetHandler(c *fiber.Ctx) error {
+	// * Parse cookie
+	cookie := c.Locals("user").(*jwt.Token)
+	claims := cookie.Claims.(*common.UserClaim)
+
+	var histories []*database.Schedule
+	result := migrations.Gorm.Table("schedules").
+		Select("transactions.name as transaction_name, transactions.date, transactions.amount, transactions.category_id as category_id, categories.name as category_name, categories.color as category_color").
+		Where("transactions.owner_id = ? AND DATE(transactions.date) = DATE(?)", claims.UserId, time.Now()).
+		Joins("left join categories on categories.id = transactions.category_id").
+		Order("transactions.date desc").
+		Scan(&histories)
+	if result.Error != nil {
+		return c.JSON(common.ErrorResponse("Error querying history information", result.Error.Error()))
+		//return *common.ErrorResponse("Error querying transaction and category information", result.Error)
+	}
+
+	return nil
+}
