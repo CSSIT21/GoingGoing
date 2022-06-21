80907ae3ce514db2de911b9d5e1cd972509928a7
Patiphon Klangpraphan <69670820+cympati@users.noreply.github.com>
2022-06-21 16:00:23 +0700

    update: patch type (#26)
    Co-Authored-By: Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
    Co-Authored-By: muumel <79404446+muumel@users.noreply.github.com>
    Co-Authored-By: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
    Co-Authored-By: Monthara Kiatruangchai <70026948+Axxmk@users.noreply.github.com>
    
    Co-authored-by: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
    Co-authored-by: Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
    Co-authored-by: muumel <79404446+muumel@users.noreply.github.com>
    Co-authored-by: Monthara Kiatruangchai <70026948+Axxmk@users.noreply.github.com>

--- backend/app/models/endpoints/party/patch_confirmed.go
+++ backend/app/models/endpoints/party/patch_confirmed.go
@@ -1,5 +1,35 @@
 package party
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"going-going-backend/app/models/common"
+	"going-going-backend/platform/database"
+	"going-going-backend/platform/database/enum"
+	"going-going-backend/platform/migrations"
+	"strconv"
+)
 
-func PatchConfirmedHandler(c *fiber.Ctx) error { return nil }
+func PatchConfirmedHandler(c *fiber.Ctx) error {
+	// * Parse cookie
+	cookie := c.Locals("user").(*jwt.Token)
+	claims := cookie.Claims.(*common.UserClaim)
+
+	// * Parse parameters
+	partyPsgId, err := strconv.ParseUint(c.Query("party_passenger_id"), 10, 64)
+	if err != nil {
+		return c.JSON(common.ErrorResponse("Unable to parse query parameter", err.Error()))
+	}
+
+	var partyPsg *database.PartyPassengers
+
+	if result := migrations.Gorm.First(&partyPsg, "passenger_id = ? AND id != ?", claims.UserId, partyPsgId).
+		Updates(
+			database.PartyPassengers{
+				Type: &enum.PartyTypes.Confirmed,
+			}).Scan(&partyPsg); result.Error != nil {
+		return c.JSON(common.ErrorResponse("Unable to update Party passengers information", err.Error()))
+	}
+
+	return c.JSON(common.SuccessResponse(&partyPsg, "Querying is success"))
+}
