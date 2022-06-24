933e66452c96e411785332cdfdae01f8ca12f3c4
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2022-06-24 13:50:21 +0700

    update: json name
    Co-Authored-By: Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
    Co-Authored-By: muumel <79404446+muumel@users.noreply.github.com>
    Co-Authored-By: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
    Co-Authored-By: Monthara Kiatruangchai <70026948+Axxmk@users.noreply.github.com>

--- backend/platform/database/location.go
+++ backend/platform/database/location.go
@@ -3,8 +3,8 @@ package database
 import "going-going-backend/platform/database/enum"
 
 type Location struct {
-	Id         *uint64         `gorm:"primaryKey"`
-	Name       *string         `gorm:"type:VARCHAR(255); not null"`
-	Address    *string         `gorm:"type:VARCHAR(255); not null"`
-	Coordinate enum.Coordinate `gorm:"type:string;"`
+	Id         *uint64         `gorm:"primaryKey" json:"id"`
+	Name       *string         `gorm:"type:VARCHAR(255); not null" json:"name"`
+	Address    *string         `gorm:"type:VARCHAR(255); not null" json:"address"`
+	Coordinate enum.Coordinate `gorm:"type:string;" json:"coordinate"`
 }
