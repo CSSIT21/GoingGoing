891f1e37b2edafb8bf5123220235f0d1bf813460
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2022-06-24 14:01:41 +0700

    update: file name
    Co-Authored-By: Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
    Co-Authored-By: muumel <79404446+muumel@users.noreply.github.com>
    Co-Authored-By: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
    Co-Authored-By: Monthara Kiatruangchai <70026948+Axxmk@users.noreply.github.com>

--- backend/platform/database/location.go
+++ backend/platform/database/location.go
@@ -1,10 +1,10 @@
 package database
 
-import "going-going-backend/platform/database/enum"
+import "going-going-backend/platform/database/array"
 
 type Location struct {
-	Id         *uint64         `gorm:"primaryKey" json:"id"`
-	Name       *string         `gorm:"type:VARCHAR(255); not null" json:"name"`
-	Address    *string         `gorm:"type:VARCHAR(255); not null" json:"address"`
-	Coordinate enum.Coordinate `gorm:"type:string;" json:"coordinate"`
+	Id         *uint64          `gorm:"primaryKey" json:"id"`
+	Name       *string          `gorm:"type:VARCHAR(255); not null" json:"name"`
+	Address    *string          `gorm:"type:VARCHAR(255); not null" json:"address"`
+	Coordinate array.Coordinate `gorm:"type:string;" json:"coordinate"`
 }
--- backend/platform/database/party_passengers.go
+++ backend/platform/database/party_passengers.go
@@ -1,12 +1,12 @@
 package database
 
-import "going-going-backend/platform/database/enum"
+import "going-going-backend/platform/database/array"
 
 type PartyPassengers struct {
-	Id          *uint64         `gorm:"primaryKey" json:"id"`
-	PartyId     *uint64         `gorm:"not null" json:"party_id"`
-	Party       *Parties        `gorm:"foreignKey:PartyId" json:"party"`
-	PassengerId *uint64         `gorm:"not null" json:"passenger_id"`
-	Passenger   *User           `gorm:"foreignKey:PassengerId" json:"passenger"`
-	Type        *enum.PartyType `gorm:"type:VARCHAR(10); default:'pending'; not null" json:"type"`
+	Id          *uint64          `gorm:"primaryKey" json:"id"`
+	PartyId     *uint64          `gorm:"not null" json:"party_id"`
+	Party       *Parties         `gorm:"foreignKey:PartyId" json:"party"`
+	PassengerId *uint64          `gorm:"not null" json:"passenger_id"`
+	Passenger   *User            `gorm:"foreignKey:PassengerId" json:"passenger"`
+	Type        *array.PartyType `gorm:"type:VARCHAR(10); default:'pending'; not null" json:"type"`
 }
