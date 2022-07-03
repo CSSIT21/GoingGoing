a43d0a32e9bf6d52144957309b2339b53470f9f6
Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
2022-07-03 22:57:38 +0700

    edit: response model 2

--- frontend/lib/models/response/common/error_info_reponse.dart
+++ frontend/lib/models/response/common/error_info_reponse.dart
@@ -1,17 +1,17 @@
 class ErrorInfoResponse {
   final bool success = false;
   final String message;
-  final String code;
+  final String? code;
   final String? error;
 
   ErrorInfoResponse({
     required this.message,
-    required this.code,
+    this.code,
     this.error,
   });
 
   ErrorInfoResponse.fromJson(Map<String, dynamic> json)
       : message = json["message"] ?? "",
-        code = json["code"] ?? "",
+        code = json["code"],
         error = json["error"];
 }
